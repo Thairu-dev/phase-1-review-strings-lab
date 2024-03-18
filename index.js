8732721818b153eb1d55561d11f7f5bfa453d868
Joseph_Thairu <joejoement@gmail.com>
2024-03-18 13:52:44 +0300

    strings lab complete

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Njeke"
+const welcomeMessage = "Welcome to Flatbook, Njeke!"
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, NJEKE!"
+const shortGreeting = "Welcome, N!"
